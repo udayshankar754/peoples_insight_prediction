e02521910457c528d185327e4c23283510fe0721
Rudara Info World <purbeyudaykumar@gmail.com>
2025-07-02 13:42:16 +0530

    changes for doing it live

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  // baseUrl: 'https://server.peoplesinsight.in/api/',
-  baseUrl: 'http://localhost:4560/api/', // connect voter dashboard local server
+  baseUrl: 'https://server.peoplesinsight.in/api/',
+  // baseUrl: 'http://localhost:4560/api/', // connect voter dashboard local server
   baseUrl_2: 'https://result.peoplesinsight.in/api/',
 };
